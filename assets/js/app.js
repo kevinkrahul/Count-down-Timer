7bd2b4943ca434640a14bc798b644758d05b00a7
kevin <kevinrahul.22ec@kct.ac.in>
2024-12-05 18:44:12 +0530

    Initial Commit

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+// countdown begin --------------------------------------
+
+var time = {
+  days: document.querySelector(".days .number"),
+  hours: document.querySelector(".hours .number"),
+  mins: document.querySelector(".mins .number"),
+  secs: document.querySelector(".secs .number")
+}
+
+// countdown setup
+
+var countdownEnd;
+
+var timeCapture = function () {
+  var now = new Date();
+  var timeLeft = countdownEnd - now; // Time difference in milliseconds
+
+  if (timeLeft > 0) {
+    // Calculate days, hours, minutes, and seconds left
+    var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    // Update the DOM
+    time.days.innerText = days;
+    time.hours.innerText = hours;
+    time.mins.innerText = minutes;
+    time.secs.innerText = seconds;
+
+    setTimeout(timeCapture, 1000); // Recursively call timeCapture every second
+  } else {
+    const contentBox = document.querySelector(".content-box");
+  contentBox.innerHTML = `
+    <div class="heading">
+      <h1>Thank You!</h1>
+    </div>
+  `;
+  }
+};
+
+
+document.querySelector(".re-ideathon").addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent default action if it's a link
+  if (!countdownEnd) { // Initialize only once
+    countdownEnd = new Date();
+    countdownEnd.setDate(countdownEnd.getDate() + 1);
+    countdownEnd.setHours(countdownEnd.getHours() + 5);
+    timeCapture(); // Start the countdown
+  }
+});
